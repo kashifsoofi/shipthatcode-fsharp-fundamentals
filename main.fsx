d2295271d6d3e35b81f9c210e0a9da8bff042691
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-09-23 20:48:30 +0100

    done

--- main.fsx
+++ main.fsx
@@ -1,9 +1,8 @@
-let minOf3 a b c =
-    let d = min a b
-    min c d
-
-let a = int(System.Console.ReadLine())
-let b = int(System.Console.ReadLine())
-let c = int(System.Console.ReadLine())
-
-printfn "%d" (minOf3 a b c)
+let line = System.Console.ReadLine()
+let result =
+    line.Split(' ')
+    |> Array.map int
+    |> Array.filter (fun x -> x % 2 = 0)
+    |> Array.map (fun x -> x * x)
+    |> Array.sum
+printfn "%d" result
