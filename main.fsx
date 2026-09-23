cbfb4c82e6cc53b0ff0c82a0062938432d73bc5f
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-09-23 21:06:46 +0100

    done

--- main.fsx
+++ main.fsx
@@ -1,7 +1,13 @@
-let rec sumList = function
-    | [] -> 0
-    | head :: tail -> head + sumList tail
+type Shape =
+    | Circle of float
+    | Square of float
 
-let line = System.Console.ReadLine()
-let nums = line.Split(' ') |> Array.map int |> Array.toList
-printfn "%d" (sumList nums)
+let area s =
+    match s with
+    | Circle r -> System.Math.PI * r * r
+    | Square s -> s * s
+
+let kind = System.Console.ReadLine()
+let dim = float (System.Console.ReadLine())
+let s = if kind = "circle" then Circle dim else Square dim
+printfn "%.2f" (area s)
