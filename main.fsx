520132f2ff0776b949d68c74b59b14acde362405
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-09-23 21:33:30 +0100

    done

--- main.fsx
+++ main.fsx
@@ -1,8 +1,4 @@
-let minMax (list: int list) : int * int =
-    (List.min(list), List.max(list))
-
 let n = int (System.Console.ReadLine())
 let nums = [for _ in 1..n -> int (System.Console.ReadLine())]
-let (mn, mx) = minMax nums
-printfn "min: %d" mn
-printfn "max: %d" mx
+let result : int = List.sum nums
+printfn "%d" result
