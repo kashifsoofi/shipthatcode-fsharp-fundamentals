ffbb836d84836f5537e73cc79c3950297ca5df95
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-09-23 20:42:40 +0100

    done

--- main.fsx
+++ main.fsx
@@ -1,4 +1,9 @@
+let minOf3 a b c =
+    let d = min a b
+    min c d
+
 let a = int(System.Console.ReadLine())
 let b = int(System.Console.ReadLine())
-let sum = a + b
-printfn "%d" sum
+let c = int(System.Console.ReadLine())
+
+printfn "%d" (minOf3 a b c)
