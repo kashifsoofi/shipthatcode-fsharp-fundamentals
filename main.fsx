124696473700a5da3633ca208cbd54daf47fde06
Kashif Soofi <kashif.soofi+git@gmail.com>
2026-09-23 21:29:08 +0100

    done

--- main.fsx
+++ main.fsx
@@ -1,10 +1,8 @@
+let minMax (list: int list) : int * int =
+    (List.min(list), List.max(list))
+
 let n = int (System.Console.ReadLine())
 let nums = [for _ in 1..n -> int (System.Console.ReadLine())]
-// TODO: use a |> pipeline with List.filter, List.map, and List.sum
-// to compute the sum of squares of the EVEN numbers in `nums`.
-let result : int =
-    nums
-    |> List.filter (fun x -> x % 2 = 0)
-    |> List.map (fun x -> x * x)
-    |> List.sum
-printfn "%d" result
+let (mn, mx) = minMax nums
+printfn "min: %d" mn
+printfn "max: %d" mx
